8e584d59bb402ab93925cfc7d928e231882e5975
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-10-13 18:37:03 +0900

    検索窓の作成

--- src/components/topPage/topMain.tsx
+++ src/components/topPage/topMain.tsx
@@ -1,6 +1,10 @@
 import React, { FC } from "react";
-import { createStyles, makeStyles } from "@material-ui/core/styles";
 import pencil from "../../assets/images/pencil.jpg";
+import Paper from "@material-ui/core/Paper";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
 
 // 背景コンポーネントのの定義
 const useStyle = makeStyles(() =>
@@ -10,13 +14,35 @@ const useStyle = makeStyles(() =>
 			backgroundSize: "cover",
 			height: "100vh",
 		},
+		paper: {
+			position: "relative",
+			marginLeft: "auto",
+			marginRight: "auto",
+			top: "33%",
+			width: "45%",
+		},
+		inputbase: {
+			width: "80%",
+		},
 	})
 );
 
 const TopMain: FC = () => {
 	const classes = useStyle();
 
-	return <div className={classes.background}></div>;
+	return (
+		<div className={classes.background}>
+			<Paper className={classes.paper}>
+				<IconButton type="submit">
+					<SearchIcon />
+				</IconButton>
+				<InputBase
+					className={classes.inputbase}
+					placeholder="検索する文字列を入力してください"
+				/>
+			</Paper>
+		</div>
+	);
 };
 
 export default TopMain;
