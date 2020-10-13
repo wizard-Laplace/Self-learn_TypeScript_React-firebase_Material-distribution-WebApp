ddaa4d71f68f846190d275f1a4dfc6b7d1548d2e
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-10-13 21:30:53 +0900

    関数を参照用にexport

--- src/pages/resultPage.tsx
+++ src/pages/resultPage.tsx
@@ -1,12 +1,14 @@
-import React, { FC } from "react";
+import { Pages } from "@material-ui/icons";
+import React, { Component, FC } from "react";
 import TopHeader from "../components/topPage/topHeader";
+import ImageItemList from "../components/resultPage/resultImageList";
 
 // 検索結果(result)コンポーネント
 const ResultPage: FC = () => {
 	return (
 		<div>
 			<TopHeader />
-			結果
+			<ImageItemList />
 		</div>
 	);
 };
