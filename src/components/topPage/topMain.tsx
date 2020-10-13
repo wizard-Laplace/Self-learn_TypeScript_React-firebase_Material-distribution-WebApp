8a01650523adf48926ad91ad6fd0d167b4237161
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-10-13 19:08:36 +0900

    検索押下時のroutingを定義

--- src/components/topPage/topMain.tsx
+++ src/components/topPage/topMain.tsx
@@ -5,6 +5,7 @@ import { createStyles, makeStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import InputBase from "@material-ui/core/InputBase";
 import SearchIcon from "@material-ui/icons/Search";
+import { useHistory } from "react-router-dom";
 
 // 背景コンポーネントのの定義
 const useStyle = makeStyles(() =>
@@ -32,14 +33,20 @@ const TopMain: FC = () => {
 	const classes = useStyle();
 	// const [値を保持する変数, 変数の値を変える関数] = useState(初期値);4
 	const [keyword, setKeyword] = useState("");
+	const history = useHistory();
 
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setKeyword(event.target.value);
 	};
 
+	// enter押下でurlを変更するロジック
+	const handleSubmit = () => {
+		history.push("/search/" + keyword);
+	};
+
 	return (
 		<div className={classes.background}>
-			<Paper className={classes.paper}>
+			<Paper className={classes.paper} component="form" onSubmit={handleSubmit}>
 				<IconButton type="submit">
 					<SearchIcon />
 				</IconButton>
